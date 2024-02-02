07c43956ad3a2ad0edc83a2a192ecaeb9d64ec87
dilaozek <97449507+dilaozek@users.noreply.github.com>
2024-02-02 23:42:22 +0300

    veri yapıları
    ders içerikleri

--- veri yapıları/agaç/TreeNode.java
+++
@@ -1,9 +0,0 @@
-class TreeNode {
-    public int data;
-    public TreeNode leftChild;
-    public TreeNode rightChild;
-    public int root;
-    public void displayNode() {
-        System.out.print("" + data+"");
-    }
-}
